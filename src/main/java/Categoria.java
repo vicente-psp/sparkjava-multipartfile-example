a987a87b3b47e06b955b488d5bfd32ff98eab011
Vicente Silva <vicente.pspenha@gmail.com>
2019-12-12 22:50:51 -0300

    Finalizado classes configuração das entidades

--- src/main/java/Categoria.java
+++ src/main/java/Categoria.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import lombok.*;
 
+import java.util.List;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
+
+    private long id;
+
+    private String name;
+
+    private List<SubCategoria> subCategorias;
+
 }
--- src/main/java/SubCategoria.java
+++ src/main/java/SubCategoria.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import lombok.*;
 
+@ToString
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
 public class SubCategoria {
+
+    private long id;
+
+    private String name;
+
 }
